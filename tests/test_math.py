64023c5f3d672d23630bc7e1cc1c6a6c91071893
noaione <noaione0809@gmail.com>
2024-10-14 19:01:38 +0700

    chore: add more tests

--- tests/test_math.py
+++ tests/test_math.py
@@ -17,6 +17,38 @@ def test_sin_math():
     assert result == 1.0
 
 
+def test_permutation():
+    result = execute_math_expr("nPr(5,2)")
+    assert result == 20.0
+
+
+def test_combination():
+    result = execute_math_expr("nCr(10,2)")
+    assert result == 45.0
+
+
+def test_power_of():
+    result = execute_math_expr("2 ** 3")
+    assert result == 8.0
+    alter = execute_math_expr("nMPr(2, 3)")
+    assert alter == 8.0
+
+
+def test_min():
+    result = execute_math_expr("min(4, 2, 2, 4, 5)")
+    assert result == 2.0
+
+
+def test_max():
+    result = execute_math_expr("max(10, 8, 20, 9, 1, 5)")
+    assert result == 20.0
+
+
+def test_absolute():
+    result = execute_math_expr("abs(-5.0)")
+    assert result == 5.0
+
+
 def test_unknown_math():
     with raises(ValueError) as err:
         execute_math_expr("PI * 2")  # It's `pi` all lowercase and not `PI`
